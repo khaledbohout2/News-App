29623eb14f49ca1df3353ad6da62b4a35531ccd6
khaledbohout2 <46874509+khaledbohout2@users.noreply.github.com>
2022-11-06 20:48:54 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Khaled Bohout on 06/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
